d32c96437131b077538ae7116cf754dc100acfa3
Caroline Moeller <caroline.moeller@rl-institut.de>
2016-06-16 15:44:55 +0200

    add some reshape examples

--- read_data.py
+++ read_data.py
@@ -6,8 +6,23 @@ Created on Fri Jun 10 13:31:23 2016
 """
 
 import pandas
+import numpy as np
 
 data = pandas.read_csv('energysystem/storage_soc.csv')
 
-print(data)
-print(data['val'])
+#print(data)
+#print(data['val'])
+
+
+# nur Beispiel
+a = np.arange(6) # erzeugt Beispiel-Array
+b = a.reshape((2,3))
+
+print(b)
+
+
+# schon unsere Daten
+
+data_matrix = data['val'].reshape((24, 365))
+
+print(data_matrix)
